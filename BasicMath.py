8b495b92c017f3d316ed99792ac4bb63cf767845
Aditya Sinha <adityasinha@Adityas-MacBook-Pro.local>
2025-06-02 21:03:43 +0530

    Assignment 1

---
+++ BasicMath.py
@@ -0,0 +1,11 @@
+num1 = float(input("Enter 1st number: "))
+num2 = float(input("Enter 2nd number: "))
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+print("")
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
